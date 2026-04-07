01286c980ea7133f11d065762c5cfc16ce6651cd
Luca Lamoni <lucalamoni@gmail.com>
2026-04-07 13:43:25 +0200

    Refactor run_app function calls to use fully qualified namespace

--- app.R
+++ app.R
@@ -3,5 +3,6 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE)
-options("golem.app.prod" = TRUE)
-run_app()
+# options("golem.app.prod" = TRUE)
+# run_app()
+fisheriesXplorer::run_app()
